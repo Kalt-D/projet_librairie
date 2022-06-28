14de62180cc118519f7e69d9b868738736d0babb
Kalt-D <yan.pruvot.pro@gmail.com>
2022-06-28 10:52:42 +0200

    Création des premiers fichiers

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+$salutations = "Huomenta, minä olen ";
+$nom = "Katt";
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Coucou</p>
+    <p> <?php echo $salutations, $nom ?> </p>
+</body>
+</html>
